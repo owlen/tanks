dea68368300262f12b7636e67e0d448a5c463288
Oren Gampel <oren@orengampel.com>
2020-06-20 11:22:58 +0300

    stripped and the bug still shows

--- game.py
+++ game.py
@@ -6,55 +6,28 @@ from wecs.panda3d import Model, Position
 # These modules contain the actual game mechanics, which we are tying
 # together into an application in this file:
 
-import movement
 import tank
 
 
 system_types = [
-    # Attach the entity's Model. This gives an entity a node as
-    # presence in the scene graph.
-    panda3d.SetupModels,
-    # Attach Geometry to the Model's node.
-    panda3d.ManageGeometry,
-    # Read player input and store it on Movement
     tank.GiveTankMoveCommands,
-    # paddles.GivePaddlesMoveCommands,
-    # Apply the Movement
-    movement.MoveObject,
-    # Did the paddle move too far? Back to the boundary with it!
-    tank.TankTouchesBoundary,
 ]
 
 
 base.ecs_world.create_entity(
-    panda3d.Model(),
-    panda3d.Geometry(file='resources/tank.bam'),
-    panda3d.Scene(node=base.render),
-    panda3d.Position(value=Vec3(-2, 10, 0)),
-    movement.Movement(value=Vec3(2, 2, 0)),
-    tank.Tank(),
-)
-
-base.ecs_world.create_entity(
-    panda3d.Model(),
-    panda3d.Geometry(file='resources/tank.bam'),
-    panda3d.Scene(node=base.render),
-    panda3d.Position(value=Vec3(-5, -20, 0)),
-    movement.Movement(value=Vec3(0, 0, 0)),
-    tank.Tank(),
+      panda3d.Model(),
+#     panda3d.Geometry(file='resources/tank.bam'),
+#     panda3d.Scene(node=base.render),
+#     panda3d.Position(value=Vec3(-5, -20, 0)),
+#     movement.Movement(value=Vec3(0, 0, 0)),
+      tank.Tank(),
 )
 
 circle = base.ecs_world.create_entity(
-    panda3d.Model(),
-    panda3d.Geometry(file='resources/circle.bam'),
-    panda3d.Scene(node=base.render),
-    panda3d.Position(value=Vec3(0, 0, 0)),
+    # panda3d.Model(),
+    # panda3d.Geometry(file='resources/circle.bam'),
+    # panda3d.Scene(node=base.render),
+    # panda3d.Position(value=Vec3(0, 0, 0)),
 )
 
 
-# the rest is to show a 10m circle
-base.ecs_world._flush_component_updates()
-
-circle[Model].node.set_scale(30)
-
-base.cam.set_pos(0, -70, 100); base.cam.look_at(circle[Position].value)
--- tank.py
+++ tank.py
@@ -1,11 +1,6 @@
-import pdb
-
-from panda3d.core import KeyboardButton
 from wecs.core import Component, and_filter
 from wecs.core import System
-from wecs.panda3d import Position, Model
-
-from movement import Movement
+from wecs.panda3d import Model
 
 
 @Component()
@@ -19,50 +14,16 @@ class GiveTankMoveCommands(System):
         'tanks': Tank
 
         # 'tanks': and_filter([
-        #     Movement,
-        #     Tank,
+        #     Model,
+        #     Tankk
         # ]),
     }
 
     def update(self, entities_by_filter):
         for entity in entities_by_filter['tanks']:
-            print(entity.get_components())
-            movement = entity[Movement]
-
-            # What keys does the player use?
-            up_key = KeyboardButton.ascii_key(b'w')
-            down_key = KeyboardButton.ascii_key(b's')
-
-            # Read player input
-            delta = 0
-            if base.mouseWatcherNode.is_button_down(up_key):
-                delta += 1
-                print(f"move {entity[Tank]}")
-            if base.mouseWatcherNode.is_button_down(down_key):
-                delta -= 1
-
-            # Store movement
-            movement.value.y = delta
+            if Tank not in entity:
+                print(f"wrong entity is given! {entity.get_components()}")
+            else:
+                print(f"Valid entity is given! {entity.get_components()}")
 
 
-class TankTouchesBoundary(System):
-    entity_filters = {
-        # 'tanks': and_filter([Position, Tank])
-        'tanks': Tank
-    }
-
-    def update(self, entities_by_filter):
-        for entity in set(entities_by_filter['tanks']):
-            # pdb.set_trace()
-            position = entity[Position]
-            print(position)
-
-            if position.value.z >= 30:
-                position.value.z = 30
-            elif position.value.z <= -30:
-                position.value.z = -30
-
-            if position.value.x >= 30:
-                position.value.x = 30
-            elif position.value.x <= -30:
-                position.value.x = -30
